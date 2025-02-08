e42ec8cd50fcc4be1660c6366631809cafe4a774
KazuhiroTakahashiAQ <kazuhiro.takahashi@asia-quest.jp>
2025-02-09 00:55:35 +0900

    エラーハンドリング修正/openapiレスポンス簡素化

--- src/routes/web/util.ts
+++ src/routes/web/util.ts
@@ -5,14 +5,14 @@ import type { AppBindings } from "./index";
 
 // Zod Exception(パラメータバリデーション)
 export const handleZodError = (
-    result: {
-            success: true;
-            data: any;
-        } | {
-            success: false;
-            error: ZodError;
-        },
-    c: Context,
+  result: {
+    success: true;
+    data: any;
+  } | {
+    success: false;
+    error: ZodError;
+  },
+  c: Context,
 ) => {
   if (!result.success) {
     return c.json(
@@ -30,31 +30,31 @@ export const handleZodError = (
 
 // HTTP Exception
 export const handleError = (err: Error, c: Context<AppBindings>): Response => {
-    if (err instanceof HTTPException) {
-        if (err.status >= 500) {
-          // Loggerを仕込んでいればここでロギング
-          console.error(`HTTPException: message: ${err.message}, status: ${err.status}`)
-        }
-        return c.json(
-            {
-              error: {
-                code: err.status,
-                message: err.message,
-              },
-            },
-            err.status
-        );
+  if (err instanceof HTTPException) {
+    if (err.status >= 500) {
+      // Loggerを仕込んでいればここでロギング
+      console.error(`HTTPException: message: ${err.message}, status: ${err.status}`)
     }
-
-    // Loggerを仕込んでいればここでロギング
-    console.error(`unhandled exception: name: ${err.name}, message: ${err.message}, cause: ${err.cause}, stack: ${err.stack}`)
     return c.json(
-        {
-            error: {
-                code: "INTERNAL_SERVER_ERROR",
-                message: err.message ?? "something unexpected happened",
-            },
+      {
+        error: {
+          code: err.status,
+          message: err.message,
         },
-        { status: 500 },
+      },
+      err.status
     );
+  }
+
+  // Loggerを仕込んでいればここでロギング
+  console.error(`unhandled exception: name: ${err.name}, message: ${err.message}, cause: ${err.cause}, stack: ${err.stack}`)
+  return c.json(
+    {
+      error: {
+        code: "INTERNAL_SERVER_ERROR",
+        message: err.message ?? "something unexpected happened",
+      },
+    },
+    { status: 500 },
+  );
 }
